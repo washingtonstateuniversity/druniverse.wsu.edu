8784e07ab55452df2b821495c423df8f45b4a776
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-02-04 12:55:41 -0800

    Use nested instead

--- includes/dru-shortcodes.php
+++ includes/dru-shortcodes.php
@@ -86,15 +86,15 @@ class dru_Shortcodes {
 
 		if ( $my_posts->have_posts() ) : while( $my_posts->have_posts() ) : $my_posts->the_post();
 		?>
-			<section class="row halves guter pad-top">
-				<div class="column one">
+			<div class="nested-halves">
+				<div class="nested-one">
 			<h1 class="blog-title"><a href="<?php the_permalink(); ?>" class="crimson"><?php echo get_the_title(); ?></a></h1>
 			<p class="blog-excerpt"><?php echo wp_trim_words( get_the_excerpt(), 72, '...' ); ?></p>
 			<p class="rmore"><a href="<?php the_permalink(); ?>">Read more</a></p>
-			<div class="column two">
-			<?php echo get_the_post_thumbnail( $post_id, 'spine-large-size'); ?>
+			<div class="nested-two">
+			<?php echo get_the_post_thumbnail( $post_id, 'spine-large_size'); ?>
 			</div>
-		</section>
+		</div>
 		<?php endwhile; endif;
 
 		wp_reset_query();
