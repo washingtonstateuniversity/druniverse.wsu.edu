dbc518007a7ed0a16313001e97d591ebaa0bab1d
jeremyfelt <jeremy.felt@gmail.com>
2015-02-04 09:24:56 -0800

    Escape tag url, slug, and name on output

--- includes/dru-shortcodes.php
+++ includes/dru-shortcodes.php
@@ -11,6 +11,7 @@ class dru_Shortcodes {
 		add_shortcode( 'search_bar', array( $this, 'search_bar_display' ) );
 		add_shortcode( 'featured_q', array( $this, 'featured_q_display' ) );
 	}
+
 	/**
 	 * Handle the display of the tag_cloud shortcode.
 	 *
@@ -19,23 +20,25 @@ class dru_Shortcodes {
 	public function tag_cloud_display() {
 		// Build the output to return for use by the shortcode.
 		ob_start();
-		?>
-<?php 
+
 		$tags = get_tags();
 		$html = '<div class="post_tags">';
-			foreach ( $tags as $tag ) {
-		$tag_link = get_tag_link( $tag->term_id );		
-		$html .= "<a href='{$tag_link}' title='{$tag->name} Tag' class='{$tag->slug}'>";
-		$html .= "{$tag->name}</a>";
-			}
+
+		foreach ( $tags as $tag ) {
+			$tag_link = get_tag_link( $tag->term_id );
+			$html .= '<a href="' . esc_url( $tag_link ) . '" title="' . esc_attr( $tag->name ) . '" class="' . esc_attr( $tag->slug ) . '">';
+			$html .= esc_html( $tag->name ) . '</a>';
+		}
+
 		$html .= '</div>';
 		echo $html;
-?>
-		<?php
+
 		$content = ob_get_contents();
 		ob_end_clean();
+
 		return $content;
 	}
+
 	/**
 	 * Handle the display of the search_bar shortcode.
 	 *
