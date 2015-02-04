9db590cf79a96026c986b37d61f4f056f6e20400
jeremyfelt <jeremy.felt@gmail.com>
2015-02-04 09:22:05 -0800

    Escape the search query used in the search form

--- includes/dru-shortcodes.php
+++ includes/dru-shortcodes.php
@@ -48,7 +48,7 @@ class dru_Shortcodes {
 		<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div>
 		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-		<input type="text" value="Search <?php echo get_search_query(); ?>" name="s" id="s" onblur="if(this.value == '') { this.value='Search <?php echo get_search_query(); ?>'}" onfocus="if (this.value == 'Search <?php echo get_search_query(); ?>') {this.value=''}"/>
+		<input type="text" value="Search <?php echo esc_attr( get_search_query() ); ?>" name="s" id="s" onblur="if(this.value == '') { this.value='Search <?php echo esc_attr( get_search_query() ); ?>'}" onfocus="if (this.value == 'Search <?php echo esc_attr( get_search_query() ); ?>') {this.value=''}"/>
 		<!--input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />-->
 	</div>
 </form>
